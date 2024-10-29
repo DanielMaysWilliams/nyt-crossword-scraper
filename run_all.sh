ed5cd731905a7665ffa073f6a64bb13e4e165643
Daniel Williams <dmw2174@columbia.edu>
2024-10-29 15:45:00 -0400

    Changed run_all script to use uv

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
-source .venv/bin/activate
-
 DATE=`date -I -d '-1 day'`
 
 echo "Beginning run_all for puzzleDate $DATE"
 
-python src/scrape.py -d $DATE
-python src/analyze.py -d $DATE
-python src/upload.py -d $DATE
+uv run src/scrape.py -d $DATE
+uv run src/analyze.py -d $DATE
+uv run src/upload.py -d $DATE
 
 echo "Finished run_all for puzzleDate $DATE"
